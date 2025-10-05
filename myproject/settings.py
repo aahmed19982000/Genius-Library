f589cf7f79c110edf052628456db449a46229cb6
ahmed <ahmedibrahim.199820@gmail.com>
2025-10-05 21:16:56 +0300

    اول نسخه من الموقع

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',        # اسم القاعدة اللي عملتها في pgAdmin
+        'USER': 'postgres',    # دلوقتي خليها postgres لحد ما نعمل مستخدم جديد
+        'PASSWORD': '1234',  # حط كلمة السر بتاعة postgres
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
